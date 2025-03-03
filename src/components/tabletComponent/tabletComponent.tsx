ff6fe2a02072bd8eb09d7e8b98677a6cfe2331ef
Luis <famorim0888@gamil.com>
2025-03-03 18:25:57 -0300

    criando o componente RegisterCustomer

--- src/components/tabletComponent/tabletComponent.tsx
+++ src/components/tabletComponent/tabletComponent.tsx
@@ -57,119 +57,119 @@ export function TabletComponent({ toStep2 }: TabletComponentProps) {
     if (cor === "Amarelo") {
       return <NotFoundItem />;
     } else {
-      return <ProductEvaluation toStep2={() => toStep2('tablet')} />;
+      return <ProductEvaluation toStep2={() => toStep2("tablet")} />;
     }
   };
 
   return (
     <div className="w-full  flex items-center justify-center">
       {showForm ? (
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>Identifique seu tablet</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4">
-              {/* Select Fabricante */}
-              <div className="flex flex-col space-y-1.5">
-                <Select
-                  onValueChange={(value) => {
-                    setSelectedFabricante(value);
-                    setSelectedModelo("");
-                    setSelectedCapacidade("");
-                    setCor("");
-                  }}
-                >
-                  <SelectTrigger id="fabricante">
-                    <SelectValue placeholder="Selecione o fabricante" />
-                  </SelectTrigger>
-                  <SelectContent position="popper">
-                    {fabricantes.map((fabricante) => (
-                      <SelectItem
-                        key={fabricante.value}
-                        value={fabricante.value}
-                      >
-                        {fabricante.label}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-              </div>
-
-              {/* Select Modelo */}
-              {selectedFabricante && (
-                <div>
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Identifique seu tablet</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="grid w-full items-center gap-4">
+                {/* Select Fabricante */}
+                <div className="flex flex-col space-y-1.5">
                   <Select
                     onValueChange={(value) => {
-                      setSelectedModelo(value);
+                      setSelectedFabricante(value);
+                      setSelectedModelo("");
                       setSelectedCapacidade("");
                       setCor("");
                     }}
                   >
-                    <SelectTrigger id="modelo">
-                      <SelectValue placeholder="Selecione o modelo" />
+                    <SelectTrigger id="fabricante">
+                      <SelectValue placeholder="Selecione o fabricante" />
                     </SelectTrigger>
                     <SelectContent position="popper">
-                      {modelos[selectedFabricante as keyof typeof modelos]?.map(
-                        (modelo, index) => (
-                          <SelectItem key={index} value={modelo}>
-                            {modelo}
-                          </SelectItem>
-                        )
-                      )}
-                    </SelectContent>
-                  </Select>
-                </div>
-              )}
-
-              {/* Select Capacidade */}
-              {selectedModelo && (
-                <div>
-                  <Select
-                    onValueChange={(value) => {
-                      setSelectedCapacidade(value);
-                      setCor("");
-                    }}
-                  >
-                    <SelectTrigger id="capacidade">
-                      <SelectValue placeholder="Selecione a capacidade" />
-                    </SelectTrigger>
-                    <SelectContent position="popper">
-                      {capacidades[
-                        selectedModelo as keyof typeof capacidades
-                      ]?.map((capacidade, index) => (
-                        <SelectItem key={index} value={capacidade}>
-                          {capacidade}
+                      {fabricantes.map((fabricante) => (
+                        <SelectItem
+                          key={fabricante.value}
+                          value={fabricante.value}
+                        >
+                          {fabricante.label}
                         </SelectItem>
                       ))}
                     </SelectContent>
                   </Select>
                 </div>
-              )}
 
-              {selectedCapacidade && (
-                <div>
-                  <Select onValueChange={handleCorChange}>
-                    <SelectTrigger id="cor">
-                      <SelectValue placeholder="Selecione a cor" />
-                    </SelectTrigger>
-                    <SelectContent position="popper">
-                      {cores[selectedCapacidade as keyof typeof cores]?.map(
-                        (cor, index) => (
-                          <SelectItem key={index} value={cor}>
-                            {cor}
+                {/* Select Modelo */}
+                {selectedFabricante && (
+                  <div>
+                    <Select
+                      onValueChange={(value) => {
+                        setSelectedModelo(value);
+                        setSelectedCapacidade("");
+                        setCor("");
+                      }}
+                    >
+                      <SelectTrigger id="modelo">
+                        <SelectValue placeholder="Selecione o modelo" />
+                      </SelectTrigger>
+                      <SelectContent position="popper">
+                        {modelos[
+                          selectedFabricante as keyof typeof modelos
+                        ]?.map((modelo, index) => (
+                          <SelectItem key={index} value={modelo}>
+                            {modelo}
                           </SelectItem>
-                        )
-                      )}
-                    </SelectContent>
-                  </Select>
-                </div>
-              )}
-            </div>
-          </form>
-        </CardContent>
-      </Card>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </div>
+                )}
+
+                {/* Select Capacidade */}
+                {selectedModelo && (
+                  <div>
+                    <Select
+                      onValueChange={(value) => {
+                        setSelectedCapacidade(value);
+                        setCor("");
+                      }}
+                    >
+                      <SelectTrigger id="capacidade">
+                        <SelectValue placeholder="Selecione a capacidade" />
+                      </SelectTrigger>
+                      <SelectContent position="popper">
+                        {capacidades[
+                          selectedModelo as keyof typeof capacidades
+                        ]?.map((capacidade, index) => (
+                          <SelectItem key={index} value={capacidade}>
+                            {capacidade}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </div>
+                )}
+
+                {selectedCapacidade && (
+                  <div>
+                    <Select onValueChange={handleCorChange}>
+                      <SelectTrigger id="cor">
+                        <SelectValue placeholder="Selecione a cor" />
+                      </SelectTrigger>
+                      <SelectContent position="popper">
+                        {cores[selectedCapacidade as keyof typeof cores]?.map(
+                          (cor, index) => (
+                            <SelectItem key={index} value={cor}>
+                              {cor}
+                            </SelectItem>
+                          )
+                        )}
+                      </SelectContent>
+                    </Select>
+                  </div>
+                )}
+              </div>
+            </form>
+          </CardContent>
+        </Card>
       ) : (
         renderValorCard()
       )}
--- src/components/verificaCPF/verificaCPF.tsx
+++ src/components/verificaCPF/verificaCPF.tsx
@@ -8,9 +8,41 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { useStep } from "@/context/StepContext";
+import { useState } from "react";
+import { RegisterCustomer } from "../registerCustomer/registerCustomer";
+import { FormField } from "../formField/formField";
+import { useToast } from "@/hooks/use-toast";
 
 export function VerificaCPF() {
+  const { toast } = useToast();
   const { setCurrentStep } = useStep();
+  const [showVerificacao, setShowVerificacao] = useState(false);
+  const [cpf, setCPF] = useState("");
+  const [showErrors, setShowErrors] = useState(false);
+
+  const handleSubmit = () => {
+    if (!cpf.trim()) {
+      setShowErrors(true);
+      toast({
+        title: "Falha!",
+        description: "Preencha os campos obrigatórios.",
+        duration: 2000,
+        className: "bg-red-500 text-white",
+      });
+      return;
+    }
+
+    toast({
+      title: "Sucesso!",
+      description: "",
+      duration: 2000,
+    });
+   setShowVerificacao(true);
+  };
+
+  if (showVerificacao) {
+    return <RegisterCustomer />;
+  }
 
   return (
     <section>
@@ -22,14 +54,23 @@ export function VerificaCPF() {
           <CardContent>
             <form>
               <div className="grid w-full items-center gap-4 text-center">
-                <Input id="cpf" placeholder="___-___-___-__" />
+                <FormField
+                  placeholder="___-___-___-__"
+                  value={cpf}
+                  onChange={(e) => setCPF(e.target.value)}
+                  required
+                  showError={showErrors}
+                />
               </div>
             </form>
           </CardContent>
           <CardFooter className="flex justify-center">
             <div className="flex flex-col gap-2">
               <div>
-                <Button className="bg-blue-700 text-white">
+                <Button
+                  className="bg-blue-700 text-white"
+                  onClick={handleSubmit}
+                >
                   Verificar existência no sistema
                 </Button>
               </div>
@@ -37,7 +78,12 @@ export function VerificaCPF() {
           </CardFooter>
         </Card>
         <div className="flex justify-start w-[450px] mt-2">
-          <Button className="bg-blue-700 text-white" onClick={() => setCurrentStep(2)}>Voltar</Button>
+          <Button
+            className="bg-blue-700 text-white"
+            onClick={() => setCurrentStep(2)}
+          >
+            Voltar
+          </Button>
         </div>
       </div>
     </section>
